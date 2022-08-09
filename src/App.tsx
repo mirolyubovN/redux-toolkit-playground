c63222064260d8ed86d1a956474f24007b9eaf14
Nikita Mirolyubov <mirolyubov_n@yahoo.co.uk>
2022-08-09 13:09:41 +0600

    innitial app setup. no rtk query used yet

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+import React, { useEffect } from 'react';
 import './App.css';
+import { useAppDispatch, useAppSelector } from './app/hooks/redux';
+import { fetchUsers } from './app/store/reducers/ActionCreators';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
-  );
+	const dispatch = useAppDispatch();
+	const {error, isLoading, users} = useAppSelector(state => state.userReducer);
+
+	useEffect(() => {
+		dispatch(fetchUsers());
+	}, [])
+
+	return (
+		<div className='text-center h-screen'>
+			{isLoading && <h1 className='text-center font-bold'>Loading...</h1>}
+			{error && <h1 className='text-center font-bold'>Error: {error}</h1>}
+			{users &&
+				<ul className='list-none text-center pt-10 mx-auto h-screen'>
+				{
+					users.map(user => (
+						<li key={user.id} className='py-2 px-4 hover:bg-gray-500 hover:text-white transition-colors flex flex-col'>
+							<span className='font-bold mr-1'>Name: {user.name}</span>
+							<span className='font-bold mr-1'>Id: {user.id}</span>
+							<span className='font-bold mr-1'>Email: {user.email}</span>
+						</li>
+					))
+				}
+				</ul>
+			}
+		</div>
+	);
 }
 
 export default App;
